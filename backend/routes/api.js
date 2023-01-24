696dbe11e7fde4713e6a65677aa102c627327504
Stephanie DelBelso <sdelbelso@grubhub.com>
2023-01-24 08:57:26 -0500

    add login logic

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const homePageRouter = require('./homePage');
 const projectRouter = require('./project');
-// const { authenticate } = require('../util/jwt');
+const { authenticate } = require('../util/jwt');
 const app = express();
 
 //TODO add 'authenticate'
-app.use('/homePage/', homePageRouter);
-app.use('/project/', projectRouter);
+app.use('/homePage/', authenticate, homePageRouter);
+app.use('/project/', authenticate, projectRouter);
 
 module.exports = app;
